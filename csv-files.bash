f85a2084b70e685d71177bd418b831703858879b
Jan Svacina <jan@svacina.net>
2019-10-21 00:36:20 -0500

    bash for navigating into database

---
+++ csv-files.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /Users/svacina/Library/Application Support/Neo4j Desktop/Application/neo4jDatabases/database-8a8fd734-5933-451b-a797-bece76e802b8/installation-3.3.2/import
+mkdir desktop-csv-import
+touch classes-methods.csv
